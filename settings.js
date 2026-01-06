9a00d55e02bbacb5178fa206af8c3593f97509a8
awolowougochukwu90-cpu <awolowougochukwu90@gmail.com>
2026-01-06 13:35:46 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'JINU-II',
   author:'ɢʟᴀᴅᴅʏ ᴋɪɴɢ',
   botName: "ᴊɪɴᴜ-ɪɪ",
-  botOwner: 'ɢʟᴀᴅᴅʏ ᴋɪɴɢ', // Your name
-  ownerNumber: '263775953409', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'awolowo', // Your name
+  ownerNumber: '+23407070879569', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
